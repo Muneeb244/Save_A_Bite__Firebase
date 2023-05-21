e8c5b1569501b150ca8361c226c9d8b03ad07420
Khadija <khadijaiqbal0123@gmail.com>
2023-05-21 09:25:14 +0500

    Verification Screen completed

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import React from 'react';
 import Login from './src/Screens/Login_SignUp/Login.js';
 import SignUp from './src/Screens/Login_SignUp/SignUp.js';
 import EmailScreen from './src/Screens/Login_SignUp/EmailScreen.js';
+import VerficationScreen from './src/Screens/Login_SignUp/VerficationScreen.js';
 
 const App = () => {
   return (
     <View>
       {/* <SignUp/> */}
       {/* <Login/> */}
-      <EmailScreen/>
+      {/* <EmailScreen/> */}
+      <VerficationScreen/>
     </View>
   )
 }
